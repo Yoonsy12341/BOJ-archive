86a48b073a1989e2bab3865abb2604f21253dc92
ysy <werq12341@naver.com>
2023-03-16 15:02:57 +0900

    요세푸스 큐

--- 1158.cpp
+++ 1158.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -28,34 +27,3 @@ int main() {
 	cout << tmp << ">\n";
 
 }
-=======
-#include <iostream>
-#include <queue>
-using namespace std;
-
-int main() {
-	int n, k, tmp;
-	cin >> n >> k;
-
-	queue<int> q;
-
-	for (int i = 1; i <= n; i++) {
-		q.push(i);
-	}
-
-	cout << "<";
-	while (q.size() > 1) {
-		for (int i = 0; i < k - 1; i++) {
-			tmp = q.front();
-			q.push(tmp);
-			q.pop();
-		}
-		tmp = q.front();
-		cout << tmp << ", ";
-		q.pop();
-	}
-	tmp = q.front();
-	cout << tmp << ">\n";
-
-}
->>>>>>> aee0d1738e9da36a02dc556fa44e428e5f40f815
